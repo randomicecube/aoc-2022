ba65781dc6cdcdc2a0fc6763508cf2513a104e1e
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-12-03 19:43:39 +0000

    Add day 2

---
+++ day-2/ex-1/aoc-2.cpp
@@ -0,0 +1,46 @@
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+
+#define MODULO_NEGATIVE(x, y) ((x % y) + y) % y
+
+enum class plays { ROCK = 1, PAPER = 2, SCISSORS = 3 };
+enum class outcomes { LOSE = 0, DRAW = 3, WIN = 6 };
+
+// clang-format off
+const std::map<char, plays> opponent = {
+  {'A', plays::ROCK},
+  {'B', plays::PAPER},
+  {'C', plays::SCISSORS}
+};
+
+const std::map<char, plays> player = {
+  {'X', plays::ROCK},
+  {'Y', plays::PAPER},
+  {'Z', plays::SCISSORS}
+};
+
+// the outcome of the game can be calculated by subtracting the player's
+// play from the opponent's play. If the result is 0, it's a draw, if it's
+// 1 or -2, the player wins, and if it's -1 or 2, the player loses.
+const std::map<int, outcomes> outcome = {
+  {0, outcomes::DRAW},
+  {1, outcomes::WIN},
+  {2, outcomes::LOSE},
+};
+// clang-format on
+
+int main() {
+  std::ifstream input{"aoc-2-input.txt"};
+  std::string line;
+  int score = 0;
+  while (std::getline(input, line)) {
+    char opponent_play, player_play;
+    sscanf(line.c_str(), "%c %c", &opponent_play, &player_play);
+    int game_outcome = (int)(player.at(player_play)) - (int)(opponent.at(opponent_play));
+    score += (int)(outcome.at(MODULO_NEGATIVE(game_outcome, 3))) + (int)(player.at(player_play));
+  }
+  std::cout << "Score: " << score << std::endl;
+  return 0;
+}
---
+++ day-2/ex-2/aoc-2.cpp
@@ -0,0 +1,54 @@
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+#define MODULO_NEGATIVE(x, y) ((x % y) + y) % y
+
+enum class plays { ROCK = 1, PAPER = 2, SCISSORS = 3 };
+enum class outcomes { LOSE = 0, DRAW = 3, WIN = 6 };
+
+// clang-format off
+const std::map<char, plays> opponent = {
+  {'A', plays::ROCK},
+  {'B', plays::PAPER},
+  {'C', plays::SCISSORS}
+};
+
+const std::map<char, outcomes> player = {
+  {'X', outcomes::LOSE},
+  {'Y', outcomes::DRAW},
+  {'Z', outcomes::WIN}
+};
+
+const std::vector<plays> helper = {
+  plays::ROCK, plays::PAPER, plays::SCISSORS,
+};
+
+// clang-format on
+
+int main() {
+  std::ifstream input{"aoc-2-input.txt"};
+  std::string line;
+  int score = 0;
+  while (std::getline(input, line)) {
+    char opponent_play, player_outcome;
+    sscanf(line.c_str(), "%c %c", &opponent_play, &player_outcome);
+    outcomes goal = player.at(player_outcome);
+    plays opponent_choice = opponent.at(opponent_play);
+    int index = (int)opponent_choice - 1;
+    switch (goal) {
+      case outcomes::LOSE:
+        score += (int)helper[MODULO_NEGATIVE(index - 1, 3)] + (int)goal;
+        break;
+      case outcomes::DRAW:
+        score += (int)helper[index] + (int)goal;
+        break;
+      case outcomes::WIN:
+        score += (int)helper[MODULO_NEGATIVE(index + 1, 3)] + (int)goal;
+    }
+  }
+  std::cout << "Score: " << score << std::endl;
+  return 0;
+}
