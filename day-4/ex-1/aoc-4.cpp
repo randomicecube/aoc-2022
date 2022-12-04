4e8dbbbaf2eebe70d889e341125b11f8f660c6a0
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-12-04 23:08:32 +0000

    Add day 4

---
+++ day-4/ex-1/aoc-4.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+
+#define FULLY_CONTAINED(a, b, c, d) ((a >= c && b <= d) || (a <= c && b >= d))
+
+int main() {
+  std::ifstream input{"aoc-4-input.txt"};
+  std::string line;
+  int score = 0;
+  while (getline(input, line)) {
+    int first_begin, first_end, second_begin, second_end;
+    sscanf(line.c_str(), "%d-%d,%d-%d", &first_begin, &first_end, &second_begin, &second_end);
+    if (FULLY_CONTAINED(first_begin, first_end, second_begin, second_end)) {
+      score++;
+    }
+  }
+  std::cout << score << std::endl;
+  return 0;
+}
---
+++ day-4/ex-2/aoc-4.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+
+// check if there's any overlap at all
+#define OVERLAP(a, b, c, d) (std::max(a, c) <= std::min(b, d))
+
+int main() {
+  std::ifstream input{"aoc-4-input.txt"};
+  std::string line;
+  int score = 0;
+  while (getline(input, line)) {
+    int first_begin, first_end, second_begin, second_end;
+    sscanf(line.c_str(), "%d-%d,%d-%d", &first_begin, &first_end, &second_begin, &second_end);
+    if (OVERLAP(first_begin, first_end, second_begin, second_end)) {
+      score++;
+    }
+  }
+  std::cout << score << std::endl;
+  return 0;
+}
