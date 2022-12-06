474b675215eb6650dfd90b1b5f0278aaf5d6113f
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-12-06 13:40:29 +0000

    Add day 6

--- day-6/ex-1/aoc-6.cpp
+++ day-6/ex-1/aoc-6.cpp
@@ -1,14 +1,32 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
-#include <string>
 #include <vector>
+#include <set>
+
+struct State {
+  std::vector<char> lastFour;
+  std::set<char> sequence;
+  int pointer = 0;
+};
 
 int main() {
   std::ifstream input("resources/aoc-input.txt");
-  std::string line;
-  while (std::getline(input, line)) {
-    // do stuff
+  struct State state;
+  char c;
+  while (state.sequence.size() != 4) {
+    if (state.lastFour.size() == 4) {
+      if (std::find(++state.lastFour.begin(), state.lastFour.end(), state.lastFour[0]) == state.lastFour.end()) {
+        state.sequence.erase(state.lastFour[0]);
+      }
+      state.lastFour.erase(state.lastFour.begin());
+    } 
+    input.get(c);
+    state.lastFour.push_back(c);
+    state.sequence.insert(c);
+    state.pointer++;
   }
+  std::cout << "Pointer: " << state.pointer << std::endl;
+  std::cout << std::endl;
   return 0;
 }
